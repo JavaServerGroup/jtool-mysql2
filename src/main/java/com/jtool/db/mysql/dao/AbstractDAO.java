fb76c08ed9ebe0b928fcb07387b976898a92766e
jiale.chan@gmail.com <jiale.chan@gmail.com>
2016-04-27 09:31:14 +0800

    jtool-mysql2 0.0.2,增加跟踪日志的基础

--- src/main/java/com/jtool/db/mysql/dao/AbstractDAO.java
+++ src/main/java/com/jtool/db/mysql/dao/AbstractDAO.java
@@ -17,9 +17,11 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.util.*;
 
+import static com.jtool.support.log.LogBuilder.buildLog;
+
 public abstract class AbstractDAO implements ApplicationContextAware {
 
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
 	protected JdbcTemplate jdbcTemplate;
 	private SimpleJdbcInsert simpleJdbcInsert;
@@ -142,9 +144,9 @@ public abstract class AbstractDAO implements ApplicationContextAware {
 			this.fields = "count(1)";
 			String sql = makeSQL();
 			Object[] params = args.toArray();
-			logger.debug("准备计算记录条数：" + sql + "\t" + Arrays.toString(params));
+			log.debug(buildLog("准备计算记录条数：" + sql + "\t" + Arrays.toString(params)));
 			int result = jdbcTemplate.queryForObject(sql, params, Integer.class);
-			logger.debug("计算记录条数为：" + result);
+			log.debug(buildLog("计算记录条数为：" + result));
 			return result;
 		}
 
@@ -156,7 +158,7 @@ public abstract class AbstractDAO implements ApplicationContextAware {
 			this.fields = "1";
 			List<Map<String, Object>> rows = AbstractDAO.this.execSelectSqlRows(makeSQL(), args.toArray());
 			boolean result = rows != null && rows.size() > 0;
-			logger.debug("是否有纪录：" + result);
+			log.debug(buildLog("是否有纪录：" + result));
 			return result;
 		}
 
@@ -192,45 +194,45 @@ public abstract class AbstractDAO implements ApplicationContextAware {
 	public <T> Optional<T> selectByPrimaryKey(Object id) {
 		try {
 			String selectByIdSQL = "select * from " + tableName + " where " + primaryKeyName + " = ?";
-			logger.debug("准备根据ID查找：" + selectByIdSQL + "\t" + id.toString());
+			log.debug(buildLog("准备根据ID查找：" + selectByIdSQL + "\t" + id.toString()));
 			@SuppressWarnings("unchecked")
 			T t = (T) jdbcTemplate.queryForObject(selectByIdSQL, makeRowMapperInstance(), id.toString());
-			logger.debug("根据ID查找到：" + t);
+			log.debug(buildLog("根据ID查找到：" + t));
 			return Optional.of(t);
 		} catch (EmptyResultDataAccessException e) {
-			logger.debug("根据ID(" + id + ")查找不到对象");
+			log.debug(buildLog("根据ID(" + id + ")查找不到对象"));
 			return Optional.empty();
 		}
 	}
 
 	public int deleteByPrimaryKey(Object id) {
 		String sql = "delete from " + tableName + " where " + primaryKeyName + " = ?";
-		logger.debug("准备根据ID删除记录：" + sql + "\t" + id);
+		log.debug(buildLog("准备根据ID删除记录：" + sql + "\t" + id));
 		int i = jdbcTemplate.update(sql, id);
-		logger.debug("删除记录条数：" + i);
+		log.debug(buildLog("删除记录条数：" + i));
 		return i;
 	}
 
 	public long addAndReturnKey(Object object) {
 		SqlParameterSource sps = new BeanPropertySqlParameterSource(object);
-		logger.debug("准备插入对象：" + object);
+		log.debug(buildLog("准备插入对象：" + object));
 		@SuppressWarnings("unchecked")
 		long id = simpleJdbcInsert.executeAndReturnKey(sps).longValue();
-		logger.debug("插入成功:" + object + "\t" + "primary key为:" + id);
+		log.debug(buildLog("插入成功:" + object + "\t" + "primary key为:" + id));
 		return id;
 	}
 
 	public void add(Object object) {
 		SqlParameterSource sps = new BeanPropertySqlParameterSource(object);
-		logger.debug("准备插入对象：" + object);
+		log.debug(buildLog("准备插入对象：" + object));
 		simpleJdbcInsert.execute(sps);
-		logger.debug("插入成功:" + object);
+		log.debug(buildLog("插入成功:" + object));
 	}
 
 	public List<Map<String, Object>> execSelectSqlRows(String sql, Object... args) {
-		logger.debug("准备查找数据：" + sql + "\t" + Arrays.toString(args));
+		log.debug(buildLog("准备查找数据：" + sql + "\t" + Arrays.toString(args)));
 		List<Map<String, Object>> result = jdbcTemplate.queryForList(sql, args);
-		logger.debug("查找到符合条件记录条数：" + result.size());
+		log.debug(buildLog("查找到符合条件记录条数：" + result.size()));
 		return result;
 	}
 
@@ -238,26 +240,26 @@ public abstract class AbstractDAO implements ApplicationContextAware {
 		try {
 			@SuppressWarnings("unchecked")
 			T t = (T) jdbcTemplate.queryForObject(sql, makeRowMapperInstance(), args);
-			logger.debug("查找到记录：" + t);
+			log.debug(buildLog("查找到记录：" + t));
 			return Optional.of(t);
 		} catch (EmptyResultDataAccessException e) {
-			logger.debug("没有查找到数据");
+			log.debug(buildLog("没有查找到数据"));
 			return Optional.empty();
 		}
 	}
 
 	public <T> List<T> execSelectSqlAsList(String sql, Object... args) {
-		logger.debug("准备查找数据：" + sql + "\t" + Arrays.toString(args));
+		log.debug(buildLog("准备查找数据：" + sql + "\t" + Arrays.toString(args)));
 		@SuppressWarnings("unchecked")
 		List<T> result = (List<T>) jdbcTemplate.query(sql, makeRowMapperInstance(), args);
-		logger.debug("查找到符合条件记录条数：" + result.size());
+		log.debug(buildLog("查找到符合条件记录条数：" + result.size()));
 		return result;
 	}
 
 	public int execUpdate(String sql, Object... args) {
-		logger.debug("执行修改操作：" + sql + "\t" + Arrays.toString(args));
+		log.debug(buildLog("执行修改操作：" + sql + "\t" + Arrays.toString(args)));
 		int result = jdbcTemplate.update(sql, args);
-		logger.debug("执行修改操作条数：" + result);
+		log.debug(buildLog("执行修改操作条数：" + result));
 		return result;
 	}
 }
